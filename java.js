54228660d76e1a3956a30834ae7664df8932e84f
MichaelSchauer <schauer.michael.ms@gmail.com>
2017-02-12 12:29:23 +0100

    ver 1.0.0

---
+++ java.js
@@ -0,0 +1,50 @@
+function openNav() {
+	document.getElementById("open").style.display="none";
+	document.getElementById("nav").style.width="100%";
+	document.getElementById("navbar").style.display="none";
+	document.getElementById("title").style.display="none";
+}
+
+function closeNav(){
+	document.getElementById("nav").style.width="0";	
+	document.getElementById("open").style.display="block";
+	document.getElementById("navbar").style.display="block";
+	document.getElementById("title").style.display="block";
+}
+
+function show(){
+	document.getElementById("stundenwh").style.display="block";
+	document.getElementById("button").style.display="none";
+	document.getElementById("button2").style.display="block"	
+}
+
+function hide(){
+	document.getElementById("stundenwh").style.display="none";
+	document.getElementById("button2").style.display="none";
+	document.getElementById("button").style.display="block";	
+}
+
+function snapcodeshow(){
+	document.getElementById("snapcode").style.display="block";
+	document.getElementById("content").style.background="black";
+	document.getElementById("content").style.filter="blur(5px)";
+	document.getElementById("content").style.background="#f1f1f1";
+}
+
+function snapcodeclose(){
+	document.getElementById("snapcode").style.display="none";
+	document.getElementById("content").style.filter="blur(0px)";
+	document.getElementById("content").style.background="#f1f1f1";
+}
+
+function zufall() {
+
+    var zahl = document.getElementById ("ende").value;
+    var zahl1 =parseInt(zahl);
+    var zahl2 = document.getElementById ("anfang").value;
+    var zahl3 =parseInt(zahl2);
+    var a = zahl3 + (zahl1-zahl3)*(Math.random());
+      a = Math.round(a);
+    var xyz = document.getElementById ("ergebnis");
+    xyz.innerHTML = a 
+;}
