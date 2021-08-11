5f959d36e5e337acad7eb3e3e576a5048dfbe1f4
MichaelSchauer <schauer.michael.ms@gmail.com>
2021-08-11 13:06:55 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -3,18 +3,17 @@ function login(){
 	var pass1="mysharedmoments";
 	password = document.getElementById("userinput").value;
 	if (password == pass1){
-    document.getElementById("homecard").style.display = "none";
     document.getElementById("infolink").style.display = "none";
-    document.getElementById("homecardunlocked").style.display = "block";
-    document.getElementById("footerboth").style.display = "flex";
-    document.getElementById("wrongpass").innerHTML = "";
-    addCookie(30);
+    document.getElementById("expanav").style.display = "block";
+    document.getElementById("footerexp").style.display = "block";
+    document.getElementById("Regenradar").setAttribute("onClick", "window.location='rainmap.html';");
+    unlockcircle();
+    document.getElementById('lockcirc').onclick = '';
 	}
 	else
    {
-    document.getElementById("wrongpass").innerHTML="FALSCHES PASSWORT";
     document.getElementById('userinput').value ='';
-    deleteAllCookies();
+    wrongpasscircle();
   }
 }
 
@@ -25,15 +24,109 @@ $('#checkmark-svg').on('click', function(){
   return false;
 })
 
-/* function openNav() {
-	document.getElementById("open").style.display = "none";
-	document.getElementById("nav").style.width = "100vw";
-	document.getElementById("title").style.display = "none";
+function opennav(){
+  document.getElementById("expanav").style.height = "100%";
+  document.getElementById("footerexp").style.height = "0px";
+  document.getElementById("closenav").style.height = "60px";
+  document.getElementById("menuitemcontainer").style.height = "320px"; //change to height of 80px x list items
 }
 
-function closeNav() {
-	document.getElementById("nav").style.width = "0";
-	document.getElementById("open").style.display = "block";
-	document.getElementById("title").style.display = "block";
-	document.getElementById("open").style.display = "inline-block";
-}  */
+function closenav(){
+  document.getElementById("expanav").style.height = "0%";
+  document.getElementById("footerexp").style.height = "60px";
+  document.getElementById("closenav").style.height = "0px";
+  document.getElementById("menuitemcontainer").style.height = "0px";
+}
+
+function unlockcircle(){
+  document.getElementById("lockcirc").style.marginLeft = "45px";
+  document.getElementById("lockcirc").style.backgroundColor = "#c7ffc7";
+ }
+
+ function wrongpasscircle(){
+  var element = document.getElementById("lockcirc");
+  element.classList.add("lockcircwrong");
+  setTimeout("removeanim()", 1000);
+ }
+
+ function removeanim() {
+  var element = document.getElementById("lockcirc");
+  element.classList.remove("lockcircwrong");
+}
+
+function openweather(){
+  document.getElementById("weatheritemcontainer").style.top = "44px";
+  document.getElementById("settings").removeAttribute("onClick", "openweather()");
+  document.getElementById("settings").setAttribute("onClick", "closeweather()");
+  spin();
+}
+
+function closeweather(){
+  document.getElementById("weatheritemcontainer").style.top = "-276px";
+  document.getElementById("settings").removeAttribute("onClick", "closeweather()");
+  document.getElementById("settings").setAttribute("onClick", "openweather()");
+  document.getElementById('spinani').className = 'spin';
+  spin();
+}
+
+function spin(){
+  var runningimg = document.getElementById("spinani");
+  runningimg.classList.add("spin");
+  setTimeout("removespin()", 1000);
+ }
+
+function removespin() {
+  var element = document.getElementById("spinani");
+  element.classList.remove("spin");
+}
+
+function hidelegend() {
+  document.getElementById("legendWeather").classList.add('legendWeatherHidden');
+  document.getElementById("legendWeather").classList.remove('legendWeatherShow');
+}
+
+function showlegend() {
+  document.getElementById("legendWeather").classList.remove('legendWeatherHidden');
+  document.getElementById("legendWeather").classList.add('legendWeatherShow');
+}
+
+function activeClouds(){
+  map.removeLayer(Rain);
+  map.removeLayer(Snow)
+  map.removeLayer(Precipitation)
+  map.addLayer(Clouds);
+  map.addLayer(Precipitation);
+  document.getElementById("activeLayerP").innerHTML="Wolken";
+  closeweather();  
+  hidelegend();
+};
+
+function activeRain(){
+  map.removeLayer(Clouds);
+  map.removeLayer(Snow)
+  map.removeLayer(Precipitation)
+  map.addLayer(Rain);
+  document.getElementById("activeLayerP").innerHTML="Regen";
+  closeweather();  
+  showlegend()
+};
+
+function activeSnow(){
+  map.removeLayer(Clouds);
+  map.removeLayer(Rain)
+  map.removeLayer(Precipitation)
+  map.addLayer(Snow);
+  document.getElementById("activeLayerP").innerHTML="Schnee";
+  closeweather();  
+  hidelegend();
+};
+
+function activePrecipitation(){
+  map.removeLayer(Clouds);
+  map.removeLayer(Snow)
+  map.removeLayer(Rain)
+  map.addLayer(Precipitation);
+  document.getElementById("activeLayerP").innerHTML="Niederschlag";
+  closeweather();  
+  hidelegend();
+};
