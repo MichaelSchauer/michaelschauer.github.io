3e110ac4ecaa6ec4e4a4ad73a3178a4d35b2da45
MichaelSchauer <schauer.michael.ms@gmail.com>
2023-02-17 12:04:30 +0100

    Update map.js

--- cams/map.js
+++ cams/map.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     var map = L.map('map').setView([48.099399, 14.250617], 8.5);
   
-    L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
     
