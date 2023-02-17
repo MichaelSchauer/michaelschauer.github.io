0a48cd398553811f06e7e529f1811a58b72a7921
MichaelSchauer <schauer.michael.ms@gmail.com>
2023-02-17 10:05:46 +0100

    Cams

--- cams/main.js
+++ cams/main.js
@@ -1,9 +1,14 @@
-function loadMap(){
-    $("#list").hide();
-    $("#map").show();
-}
+function loadCams(){
+    d3.json("res/stations.json").then(function(data_json) {
+        console.log(data_json)
+        for(var i = 0; i < data_json.length; i++){
+            var url = JSON.stringify(data_json[i].url);
+            var name = data_json[i].name;
+            $("#list").append("<card><div class='image' style='background-image: url("+data_json[i].url+");' onclick='window.location = "+url+"'></div><p>"+name+"</p></card>");
 
-function loadList(){
-    $("#list").show();
-    $("#map").hide();
+            // <div class='image' style='background-image: url("+data_json[i].url+");' onclick='window.location = "+data_json[i].url+";></div>
+        }
+    })
+   
+
 }
